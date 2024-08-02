19279904c932b31829d183d23abc453c9a2c6778
GF Blue <gfblue47@gmail.com>
2024-08-02 17:48:11 +0500

    my-project

---
+++ increment/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Increment from './Components/Increment';
+
+function App() {
+  return (
+   <>
+   <Increment></Increment>
+   </>
+  );
+}
+
+export default App;
---
+++ increment/src/Components/Increment.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './incrent.css';
+
+function Increment() {
+
+    const [num, setNum] = useState(0)
+
+    const numInc = () =>{
+        setNum(num + 1);
+    }
+
+    const numDec = () =>{
+if (num > 0){
+    setNum(num - 1);
+}else{
+    alert("sorry, zero number reached")
+    setNum(0);
+}
+    }
+    return (
+        <>
+        <div className="mainDiv">
+            <div className="centerDiv">
+                <h1>{num}</h1>
+                <div className="btnDiv"></div>
+                <button onClick={numInc}> Increment</button>
+                <button onClick={numDec}> Decrement</button>
+
+            </div>
+
+        </div>
+
+
+        </>
+     
+    );
+  }
+  
+  export default Increment;
